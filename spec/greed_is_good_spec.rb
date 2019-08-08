cc406af70e79caf1869baadcf86e9c6ed05b2ca6
btmccollum <btmcco8@gmail.com>
2019-08-08 16:22:35 -0500

    adding start of new kata and test

---
+++ spec/greed_is_good_spec.rb
@@ -0,0 +1,10 @@
+require_relative './Ruby/greed_is_good'
+
+describe '#score' do
+  describe 'takes in an array of dice rolls and returns a score' do
+    it 'should return 0 when passed [2, 3, 4, 6, 2]' do
+      dice_rolls =  [2, 3, 4, 6, 2]
+      expect(score(dice_rolls)).to eq(0)
+    end
+  end
+end
